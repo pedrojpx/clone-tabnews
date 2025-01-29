162cf84595e1d8bca8f0debb6d0bd1970d7a575b
pedrojpx <pjpxavier@gmail.com>
2025-01-29 11:04:58 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olarrr, isso é só testezinho de um curso que eu tô fazendo aqui. Espero que até o momento em que eu te mandar isto, seus cristais já tenham alinhado hahaha</h1>
+    return <h1>Olarrr, isso é só testezinho de um curso que eu tô fazendo aqui. Espero que até o momento em que eu te mandar isto, seus cristais do ouvido já tenham se alinhado hahaha</h1>
 }
 
 export default Home;
