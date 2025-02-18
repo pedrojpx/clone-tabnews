3b1da0c0be9654811151cf78dc3cc7d23337574c
pedrojpx <pjpxavier@gmail.com>
2025-02-18 17:16:56 -0300

    refactored `/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,14 +2,14 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
 
   const dbVersion = await database.query("SHOW server_version;");
   const maxConnections = await database.query("show max_connections;");
-  const openedConnections = await database.query(
-    "SELECT * FROM pg_stat_activity where datname = 'local_db';",
-  );
-
-  console.log(openedConnections);
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
 
   response.status(200).json({
     updated_at: updatedAt,
@@ -17,7 +17,7 @@ async function status(request, response) {
       database: {
         version: dbVersion.rows[0].server_version,
         max_connections: parseInt(maxConnections.rows[0].max_connections),
-        open_connections: openedConnections.rows.length,
+        open_connections: openedConnections.rows[0].count,
       },
     },
   });
