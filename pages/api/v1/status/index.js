7cbf60c3e5bfab0fe3c86ba0984cee2b711c4349
pedrojpx <pjpxavier@gmail.com>
2025-02-18 16:32:49 -0300

    implemented status page

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("show max_connections;");
+  const openedConnections = await database.query(
+    "SELECT * FROM pg_stat_activity where datname = 'local_db';",
+  );
+
+  console.log(openedConnections);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        open_connections: openedConnections.rows.length,
+      },
+    },
+  });
 }
 
 export default status;
